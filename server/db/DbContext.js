d4f89f8116ec509b4bd241e8d94ffe4258f22489
Steffen Larson <steffman13@gmail.com>
2021-01-13 16:07:27 -0700

    put models into dbcontext. Created serv/controls

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,8 +1,22 @@
 import ValueSchema from "../models/Value";
+
+
 import mongoose from "mongoose";
+
+import GalaxySchema from "../models/Galaxy";
+import StarSchema from "../models/Star";
+import PlanetSchema from "../models/Planet";
+import MoonSchema from "../models/Moon";
+
+
 
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
+  Galaxy = mongoose.model("Galaxy", GalaxySchema);
+  Star = mongoose.model("Star", StarSchema);
+  Planet = mongoose.model("Planet", PlanetSchema);
+  Moon = mongoose.model("Moon", MoonSchema)
+
 }
 
 export const dbContext = new DbContext();
